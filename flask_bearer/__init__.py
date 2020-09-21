90175adfa482d12f4608e4fcc6ea8235f8407e18
mfrancis95 <mikefrancis95@gmail.com>
2020-09-21 02:48:40 -0400

    Add pylint requirement

--- flask_bearer/__init__.py
+++ flask_bearer/__init__.py
@@ -1,13 +1,32 @@
+'''flask-bearer
+
+This module provides a class that can be used to make Flask routes require
+bearer authentication tokens.
+
+'''
+
+# pylint: disable=R0903
+
 from functools import wraps
 from flask import abort, request
 
 class BearerToken:
+    '''Class used to make Flask routes require bearer authentication tokens.'''
 
-    def __init__(self, token, unauthorized = lambda: abort(401)):
+    def __init__(self, token, unauthorized=lambda: abort(401)):
         self._token = token
         self._unauthorized = unauthorized
 
     def required(self, function):
+        '''Decorator function of a Flask route that makes it require a bearer
+        authentication token.
+
+        The function only succeeds if the request header contains the
+        Authorization field and if the value of the field is equal to
+        "Bearer <token>".
+
+        Otherwise, the function will abort.
+        '''
         @wraps(function)
         def decorated_function(*args, **kwargs):
             authorization = request.headers.get('Authorization', '')
