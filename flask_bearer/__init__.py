58a614b4ae84936be85b0f8528500719f795cb82
mfrancis95 <mikefrancis95@gmail.com>
2020-11-26 01:08:57 -0500

    Check Authorization field using an fstring comparison

--- flask_bearer/__init__.py
+++ flask_bearer/__init__.py
@@ -13,7 +13,7 @@ from flask import abort, request
 class BearerToken:
     '''Class used to make Flask routes require bearer authentication tokens.'''
 
-    def __init__(self, token, unauthorized=lambda: abort(401)):
+    def __init__(self, token: str, unauthorized=lambda: abort(401)):
         self._token = token
         self._unauthorized = unauthorized
 
@@ -30,9 +30,7 @@ class BearerToken:
         @wraps(function)
         def decorated_function(*args, **kwargs):
             authorization = request.headers.get('Authorization', '')
-            if not authorization.startswith('Bearer '):
-                return self._unauthorized()
-            if authorization[7:] != self._token:
+            if authorization != f'Bearer {self._token}':
                 return self._unauthorized()
             return function(*args, **kwargs)
         return decorated_function
